462e72b2de53509a16b1dec57fcbd2c9eb7c5112
Frank <Frank>
2017-01-09 19:18:35 -0500

    ok

--- www/activity/detalleNaveEmpresaActivity.js
+++ www/activity/detalleNaveEmpresaActivity.js
@@ -161,7 +161,7 @@ var detalleNaveEmpresaActivity =
                     }
                 });
                 
-                var htmlShip = '<div style="overflow: auto;height:100%" id="divAIS">' + htmlDataShip + '</div>';
+                var htmlShip = '<div class="page-content hide-bars-on-scroll" id="divAIS">' + htmlDataShip + '</div>';
                 $$("#tabnaveais").html(htmlShip);
             }
         );
@@ -196,7 +196,7 @@ var detalleNaveEmpresaActivity =
                     }
                 });
                 
-                var htmlShipCompany = '<div style="overflow: auto;height:100%" id="divShipCompany">' + htmlDataShipCompany + '</div>';
+                var htmlShipCompany = '<div class="page-content hide-bars-on-scroll" id="divShipCompany">' + htmlDataShipCompany + '</div>';
                 $$("#tabnaveinformation").html(htmlShipCompany);
             }
         );
