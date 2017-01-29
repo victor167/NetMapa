3654ecaaf172146c34e3a65f274d68445e745307
Victor Recines <victor@Apple-5.local>
2017-01-29 04:11:54 -0500

    ok

--- www/activity/indexActivity.js
+++ www/activity/indexActivity.js
@@ -29,7 +29,8 @@ var div;
 var indexActivity = 
 {
     ini: function(){
-        div = document.getElementById("map_canvas3");
+        div = $$("#map_canvas3")[0];
+        //div = document.getElementById("map_canvas3");
         map = plugin.google.maps.Map.getMap(div);
         map.one(plugin.google.maps.event.MAP_READY, function() {
             console.re.log("map ready");
