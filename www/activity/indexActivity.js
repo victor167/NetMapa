d26a02e3f372debb213ea8d4a7a3187aa3479636
Victor Recines <victor@Apple-5.local>
2017-01-29 15:11:41 -0500

    ok

--- www/activity/indexActivity.js
+++ www/activity/indexActivity.js
@@ -31,12 +31,13 @@ var indexActivity =
     ini: function(){
 
         
-          //div = document.getElementById("map_canvas3");
-          div = $$("#map_canvas3")[0];
-          map = plugin.google.maps.Map.getMap(div);
-          map.one(plugin.google.maps.event.MAP_READY, function() {
-            map_ready();
-          });
+      //div = document.getElementById("map_canvas3");
+      div = $$("#map_canvas3")[0];
+      map = plugin.google.maps.Map.getMap(div);
+      map.one(plugin.google.maps.event.MAP_READY, function() {
+        alert("map_ready");
+        map_ready();
+      });
     
 
         var POINTS = 
